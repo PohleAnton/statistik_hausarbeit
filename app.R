1180e54e76c65c996ed676c80ebd8272e49e0429
PohleAnton <s0582388@htw-berlin.de>
2022-12-19 20:06:07 +0100

    Impfungen/Infektionen pro Woche geadded

--- app.R
+++ app.R
@@ -18,6 +18,54 @@ data<-read.csv2("./Data/RKI_COVID19_Berlin.csv", header=T, sep=",")
 data$Meldedatum<-as.Date(data$Meldedatum)
 data$Refdatum<-as.Date(data$Refdatum)
 
+##Daten über die Impfkampangne vom RKI, sortiert nach bundesländern:
+##https://github.com/robert-koch-institut/COVID-19-Impfungen_in_Deutschland/blob/master/Aktuell_Deutschland_Bundeslaender_COVID-19-Impfungen.csv
+
+impfungen<-read.csv2("./Data/impfungen.csv", header = T, sep = ",")
+
+##aus den zurückgehenden Todesfällen v.a. bei Delta im Vergleich zu den weniger letalen Vorgängern ergibt sich ein Interesse für die Impfkampagne:
+
+##Impfungen in Berlin:
+##hier wird nur der Landkreis des Impfortes angegeben, nicht der Meldeadresse der geimpften Person. (Vermutlich aus Datenschutzgründen)
+impfungen_b<-impfungen[impfungen$BundeslandId_Impfort==11,]
+
+##in 2021 
+impf21<-impfungen_b[impfungen_b$Impfdatum>'2020-12-31' & impfungen_b$Impfdatum<'2022-01-01',]
+##fügt wochenspalte hinzu
+impf21$woche<-strftime(impf21$Impfdatum, format="%V")
+##remove leading zeros von "woche":
+impf21$woche<-sub("^0+","",impf21$woche)
+##Daten gesamt in 2021
+data21<-data[data$Meldedatum>'2020-12-31' & data$Meldedatum<'2022-01-01',]
+data21$woche<-strftime(data21$Meldedatum, format="%V")
+##remove leading zeros von "woche":
+data21$woche<-sub("^0+","",data21$woche)
+
+
+
+##in 2022
+impf22<-impfungen_b[impfungen_b$Impfdatum>'2021-12-31',]
+##fügt eine wochenspaltehinzu
+impf22$woche<-strftime(impf22$Impfdatum, format="%V")
+##remove leading zeros von "woche":
+impf22$woche<-sub("^0+","",impf22$woche)
+##Daten gesamt in 2022
+data22<-data[data$Meldedatum>'2021-12-31',]
+##fügt eine Wochenspalte hinzu
+data22$woche<-strftime(data22$Meldedatum, format="%V")
+##remove leading zeros von "woche":
+data22$woche<-sub("^0+","",data22$woche)
+
+sum(impf21$Anzahl[])
+barplot(sum(impf21$Anzahl[impf21$woche==15]),ylim=c(0,300000))
+
+sum(impf21$Anzahl[impf21$woche=="05"])
+
+##es wird im weiteren nach totalen Impfdosen verabreicht ermittelt, nicht nach Anzahl der erhaltenen Impfungen
+##2020 wird wegen den quasi noch nicht verfügbaren Impfdosen nicht beachtet
+
+
+
 ## gemäß
 ## https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/VOC_VOI_Tabelle.xlsx?__blob=publicationFile
 ## wird folgendes angenommen:
@@ -38,28 +86,45 @@ ui <- fluidPage(
     # Application title
     titlePanel("Covid19 in Berlin"),
   
+    sidebarPanel(
+      selectInput("Altersgruppe", label="Wählen Sie eine Altersgruppe", choices = list("unbekannt"=1, "0 bis 4 Jahre"=2,"5 bis 14 Jahre"=3, "15 bis 34 Jahre"=4,"35 bis 59 Jahre"=5,"60 bis 79 Jahre"=6,"über 80 Jahre"=7, "Gesamt"=8), selected = 8),
+      radioButtons("Variante", label="Welche Virusvariante soll betrachtet werden?", choices = list("Urtyp"=1, "Alpha"=2, "Delta"=3, "Omikron"=4, "Alle zusammen"=5), selected = 5),
+      sliderInput(inputId="Woche", label="Kalenderwoche", min=1, max=53, value=1),
+      radioButtons("Jahr", label="Welches Kalenderjahr soll betrachtet werden?", choices=list("2021"=1, "2022"=2), selected=1)
+    ),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-      
-        sidebarPanel(
-          selectInput("Altersgruppe", label="Wählen Sie eine Altersgruppe", choices = list("unbekannt"=1, "0 bis 4 Jahre"=2,"5 bis 14 Jahre"=3, "15 bis 34 Jahre"=4,"35 bis 59 Jahre"=5,"60 bis 79 Jahre"=6,"über 80 Jahre"=7, "Gesamt"=8), selected = 8),
-          radioButtons("Variante", label="Welche Virusvariante soll betrachtet werden?", choices = list("Urtyp"=1, "Alpha"=2, "Delta"=3, "Omikron"=4, "Alle zusammen"=5), selected = 5)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          textOutput("TextAlter"),
-          plotOutput("VerhaeltnisAlter"),
+mainPanel(
+  plotOutput("VerhaeltnisAlter"),
+  textOutput("TextAlter"),
+  plotOutput("impfungen_woche"),
+  plotOutput("faelle_woche")
+  
 
 
-        )
-    )
-)
+))
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
+  var_woche<-reactive({
+    return(input$Woche)
+  })
+  
+  ##gibt den Datensatz für das gewählte Kalenderjahr zurück:
+  ##gibt einen vektor mit 2 datensätzen zurück:
+  ##var_jahr()[1]: der gesamtdatensatz mit wochen
+  ##var_jahr()[2]: die impfdaten mit wochen
+  var_jahr<-reactive({
+    if(input$Jahr==1)({
+      return (c(sum(data21$AnzahlFall[data21$woche==var_woche()]), sum(impf21$Anzahl[impf21$woche==var_woche()])))
+    })
+    if(input$Jahr==2)({
+      return (c(sum(data22$AnzahlFall[data22$woche==var_woche()]), sum(impf22$Anzahl[impf22$woche==var_woche()])))
+    })
+  })
+  
+  output$impfungen_woche<-renderPlot(barplot(main="Anzahl Impfungen in dieser KW",var_jahr()[2],ylim=c(0,270000)))
+  output$faelle_woche<-renderPlot(barplot(main="Anzahl Infektionen in dieser KW",var_jahr()[1],ylim=c(0,90000)))
+                             
   ##gibt reduzierte Datensätze zurück - im jeweiligen Zeitraum war die Variante mit >50% vertreten
   var_variant<-reactive({
     if(input$Variante==1)({
